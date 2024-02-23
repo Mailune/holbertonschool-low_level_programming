a5b8988a42e1b9f08ea068262d52f77276f87f5f
Mailune <mailysrosereau@yahoo.fr>
2024-02-23 10:55:30 +0100

    Write a program that prints the lowercase alphabet in reverse, followed by a new line

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print last digit
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	char letter;
+
+	for (letter = 'z'; letter <= 'a'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
