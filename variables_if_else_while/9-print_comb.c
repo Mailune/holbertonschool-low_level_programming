6daf13e200fcaf6dd74ce7a0ce01a3bbe3e2fc5b
Mailune <mailysrosereau@yahoo.fr>
2024-02-24 17:10:48 +0100

    program that prints all possible combinations of single-digit numbers

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - print all combinations of single digit
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit <= 9; digit++)
+	{
+		putchar(digit);
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
