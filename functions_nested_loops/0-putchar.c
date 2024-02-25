77d5b727f8da8b1f7e8a831d45f058afa30d0c4e
Mailune <mailysrosereau@yahoo.fr>
2024-02-25 02:51:33 +0100

    program that prints _putchar, followed by a new line

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * main - print _putchar
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char word[9] = "_putchar";
+	int p;
+
+	for (p = 0; p <= 9; p++)
+	{
+		_putchar(word[p]);
+	_putchar('\n');
+	}
+	return 0;
+}
+
