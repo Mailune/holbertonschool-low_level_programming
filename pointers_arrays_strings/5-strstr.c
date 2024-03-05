382d5d04c75bbccbd75cbd86420e0057c4065c0f
Mailune <mailysrosereau@yahoo.fr>
2024-03-05 19:30:25 +0100

    correction for check the characters from needle in haystack starting from the index position

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -14,12 +14,20 @@ char *_strstr(char *haystack, char *needle)
 		for (index = 0; haystack[index] != '\0'; index++)
 		{
 			if (haystack[index] == needle[0])
+			{
 
-			for (needle_index = 0; needle[needle_index] != '\0'; needle_index++)
+				for (needle_index = 0; needle[needle_index] != '\0'; needle_index++)
+				{
+					if (haystack[index + needle_index] != needle[needle_index])
+					{
+					}
+				}
+				if (needle[needle_index] == '\0')
+				{
 
-			if (haystack[index] == needle[needle_index])
-
-				return (&needle[needle_index]);
+				return (&haystack[index]);
+				}
+			}
 		}
 		return (NULL);
 }
