08a12dd87638dbf289d52cbdf5a6d351cc3d92a0
Mailune <mailysrosereau@yahoo.fr>
2024-03-08 15:22:29 +0100

    to do

---
+++ argc_argv/4-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - adds positive numbers
+ *@argc: int argument
+ *@argv: char type pointer argument
+ *Return: the result followed by new line
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	(void)argc;
+}
