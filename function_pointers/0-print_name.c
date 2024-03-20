23691f668d7cf0cf6fac631db2992e9fccb3fb50
Mailune <mailysrosereau@yahoo.fr>
2024-03-20 13:12:06 +0100

    correction ajout verification si parametres de la fonction sont valides

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "function_pointers.h"
 /**
  *print_name - prints a name
@@ -7,9 +8,7 @@
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
-	{
 		return;
-	}
 
 	f(name);
 }
