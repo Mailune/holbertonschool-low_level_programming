4a89e70a0917253b44d969accc9999e8fd06c30c
Mailune <mailysrosereau@yahoo.fr>
2024-03-20 13:57:20 +0100

    contient les 5 fonctions de calculs

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+/**
+ * op_add - Function that returns the sum of two integers
+ *@a: First integer
+ *@b: Second integer
+ *
+ * Return: Sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Function that returns the difference of two integers
+ *@a: First integer
+ *@b: Second integer
+ *
+ * Return: Difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Function that returns the product of two integers
+ *@a: First integer
+ *@b: Second integer
+ *
+ * Return: Product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Function that returns the result of the division of two integers
+ *@a: First integer
+ *@b: Second integer
+ *
+ * Return: Result of the division of a by b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Function that returns the remainder of the division of two integers
+ *@a: First integer
+ *@b: Second integer
+ *
+ * Return: Remainder of the division of a by b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
