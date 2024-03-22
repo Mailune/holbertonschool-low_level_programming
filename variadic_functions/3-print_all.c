3dbe74430ccb4e0ae5cd63580e694007263230a3
Mailune <mailysrosereau@yahoo.fr>
2024-03-22 19:06:33 +0100

    correction requierement no else

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -7,43 +7,42 @@
  */
 void print_all(const char * const format, ...)
 {
-        va_list args;
-        int i = 0;
-        char *str;
+	va_list args;
+	int i = 0;
+	char *str;
 
-        va_start(args, format);
+	va_start(args, format);
 
-        while (format != NULL && format[i] != '\0')
-        {
-                switch (format[i])
-                {
-                        case 'c':
-                                printf("%c", va_arg(args, int));
-                                break;
-                        case 'i':
-                                printf("%d", va_arg(args, int));
-                                break;
-                        case 'f':
-                                printf("%f", va_arg(args, double));
-                                break;
-                        case 's':
-                                str = va_arg(args, char *);
-                                if (str == NULL)
-                                        printf("(nil)");
-                                else
-                                        printf("%s", str);
-                                        break;
-                        break;
+	while (format != NULL && format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (str == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", str);
+					break;
+		}
+				printf(", ");
+			i++;
+	}
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+		&& format[(i + 1)] != '\0')
 
-                }
-                if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||
-                 format[i] == 's') && format[i + 1] != '\0')
-                        printf(", ");
-                i++;
-        }
+	printf("\n");
 
-        printf("\n");
-
-        va_end(args);
+	va_end(args);
 }
-
