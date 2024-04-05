369b8622d2bc6bc7453e945879d45b0fc4d20130
Mailune <mailysrosereau@yahoo.fr>
2024-04-06 01:46:02 +0200

    change of syntax in the if condition

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -12,10 +12,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new_node;
 
 	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-	{
+	if (!new_node)
 		return (NULL);
-	}
 
 	new_node->n = n;
 	new_node->prev = NULL;
@@ -23,9 +21,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	if (*head)
 		(*head)->prev = new_node;
-		*head = new_node;
+	*head = new_node;
 
 	return (new_node);
-
-
 }
